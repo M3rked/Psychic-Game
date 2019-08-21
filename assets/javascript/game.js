f390951dfa978b6d6e55e77640185f935f81ad94
marvin lara <laramarvin105@gmail.com>
2019-08-21 16:10:30 -0400

    updated game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,37 @@
 
-var randomNumber= getRandomNumber(100);
-
-var guess;
-
+var randomNumber= Math.floor(Math.random() * 10) +1;
 
 var guessCount = 0;
 var correctGuess = true;
+var incorrectGuess= false
+var guess = prompt("Guess a number from 1 - 10")
+var maxTries = 5; 
 
 
 
+while (guess !== randomNumber ){
+    var guess = prompt("Guess a number from 1 - 10");
+    guessCount +=1 ; 
 
 
-function getRandomNumber(upper){
-    var  num =Math.floor(Math.random() * 100) +1; 
-    console.log(num);
-    return num; 
+    if (guessCount > maxTries){
+        
+        document.write('You have no more tries left');  
+
+        break
+    }
+
+
+    if ( guess === randomNumber){
+        document.write('You guessed correctly');
+        
+        then($("correct").append(guessCount))
+        console.log("Guesses", guess)
+    }
+
     
 }
-while(true){
-    guess = prompt("Guess a number from 1 - 100");
-    guessCount +=1;
 
-
-
-if (parseInt(guess) == randomNumber) {
-    correctGuess = true;
-
-    document.write ("you guessed correctly");
-    break; 
-}
-
-else {document.write("you guessed wrong")
-    break;
-}  
-
-}
-
-document.write('<h1> you guessed correctly</h1>')
-document.write('you took' + guessCount); 
 
 
 
